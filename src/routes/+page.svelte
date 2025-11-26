d8f8d801fd89c667172232d1016a74572f66bce4
hactuss <157395485+hactuss@users.noreply.github.com>
2025-11-26 15:05:52 +0100

    Add files via upload

---
+++ src/routes/+page.svelte
@@ -0,0 +1,91 @@
+<script>
+	let uid = $state();
+	let progress3 = $state(0);
+
+	// setInterval((uid = Math.floor(Math.random() * 100)), 1000);
+	const list = ['moin', 'hello', ':)', 'search for anything'];
+
+	function resolveAfter2Seconds() {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				//resolve(Math.floor(Math.random() * 100));
+				resolve(list[Math.floor(Math.random() * list.length)]);
+			}, 100);
+		});
+	}
+	function progressbar() {
+		let progress2 = 0;
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				resolve(Math.floor(Math.random() * 100));
+			}, 100);
+		});
+	}
+
+	async function asyncCall() {
+		const result = await resolveAfter2Seconds();
+		progress3 = await progressbar();
+		progress3 = await progressbar();
+		progress3 = await progressbar();
+		progress3 = 100;
+		uid = result;
+	}
+	function capped() {
+		asyncCall();
+	}
+	capped();
+	let input = '';
+	function handleKeydown(event) {
+		if (event.key == 'Enter') {
+			console.log(input);
+		} else {
+			return null;
+		}
+	}
+	function presssearch() {
+		console.log(input);
+	}
+</script>
+
+<svelte:window onkeydown={handleKeydown} />
+<main id="MAIN_CONTAINER">
+	<img src="https://upload.wikimedia.org/wikipedia/commons/f/f0/Compactcassette.jpg" alt="bruh" />
+	<p>hactuss</p>
+	<h1>{uid}</h1>
+	<div id="SEARCH_BOX">
+		<input type="text" contextmenu="hello" bind:value={input} />
+		<button onclick={presssearch}>Search</button>
+	</div>
+	<br />
+	{#await resolveAfter2Seconds()}
+		<p>waiting...</p>
+	{:then uid}
+		<p>here you go, uid is {uid}</p>
+	{/await}
+
+	<div id="loadingbar">
+		<div id="progress" style:width={progress3 + '%'}>{progress3}%</div>
+	</div>
+</main>
+
+<style>
+	#MAIN_CONTAINER {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 99vh;
+	}
+	#SEARCH_BOX {
+		background-color: rgba(0 0 200 0.5);
+	}
+	#loadingbar {
+		height: 1rem;
+		width: 100px;
+		background-color: red;
+	}
+	#progress {
+		height: 1rem;
+		background-color: blue;
+	}
+</style>
